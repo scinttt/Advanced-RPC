7c6c35d8dcc9ad47ed1b8b28d495e5897320df76
scinttt <scinttt@gmail.com>
2025-04-03 18:29:43 -0400

    Implement Json Serializer and Serializer SPI

---
+++ rpc-core/src/main/java/com/creaturelove/serializer/JsonSerializer.java
@@ -0,0 +1,61 @@
+package com.yupi.yurpc.serializer;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.creaturelove.model.RpcRequest;
+import com.creaturelove.model.RpcResponse;
+import com.creaturelove.serializer.Serializer;
+
+import java.io.IOException;
+
+/**
+ * Json 序列化器
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @learn <a href="https://codefather.cn">编程宝典</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+public class JsonSerializer implements Serializer {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    @Override
+    public <T> byte[] serialize(T obj) throws IOException {
+        return OBJECT_MAPPER.writeValueAsBytes(obj);
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> classType) throws IOException {
+        T obj = OBJECT_MAPPER.readValue(bytes, classType);
+        if(obj instanceof RpcRequest){
+            return handleRequest((RpcRequest) obj, classType);
+        }
+        if(obj instanceof RpcResponse){
+            return handleResponse((RpcResponse) obj, classType);
+        }
+        return obj;
+    }
+
+    // object 原始对象会被擦除，deserialization 是会被当做 linkedHashMap 回不去原始对象，这里进行额外处理
+    private <T> T handleRequest(RpcRequest rpcRequest, Class<T> type) throws IOException{
+        Class<?>[] parameterTypes = rpcRequest.getParameterTypes();
+        Object[] args = rpcRequest.getArgs();
+
+        // loop handle every args type
+        for(int i=0; i<parameterTypes.length; i++){
+            Class<?> clazz = parameterTypes[i];
+            // if type is different, rehandle the type
+            if(!clazz.isAssignableFrom(args[i].getClass())){
+                byte[] argBytes = OBJECT_MAPPER.writeValueAsBytes(args[i]);
+                args[i] = OBJECT_MAPPER.readValue(argBytes, clazz);
+            }
+        }
+        return type.cast(rpcRequest);
+    }
+
+    private <T> T handleResponse(RpcResponse rpcResponse, Class<T> type) throws IOException{
+        // handle response data
+        byte[] dataBytes = OBJECT_MAPPER.writeValueAsBytes(rpcResponse.getData());
+        rpcResponse.setData(OBJECT_MAPPER.readValue(dataBytes, rpcResponse.getDataType()));
+        return type.cast(rpcResponse);
+    }
+}
