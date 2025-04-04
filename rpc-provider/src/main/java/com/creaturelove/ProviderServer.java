aac14377fe07b965f8c8cac8e9b6e1e37143c3a6
scinttt <scinttt@gmail.com>
2025-04-04 17:35:35 -0400

    Let provider register its service to the Registry Center during initialization

--- rpc-provider/src/main/java/com/creaturelove/ProviderServer.java
+++ rpc-provider/src/main/java/com/creaturelove/ProviderServer.java
@@ -1,6 +1,11 @@
 package com.creaturelove;
 
+import com.creaturelove.config.RegistryConfig;
+import com.creaturelove.config.RpcConfig;
+import com.creaturelove.model.ServiceMetaInfo;
 import com.creaturelove.registry.LocalRegistry;
+import com.creaturelove.registry.Registry;
+import com.creaturelove.registry.RegistryFactory;
 import com.creaturelove.server.HttpServer;
 import com.creaturelove.server.VertxHttpServer;
 import com.creaturelove.service.UserService;
@@ -17,8 +22,24 @@ public class ProviderServer
         //RPC Initialization
         RpcApplication.init();
 
-        // register the service
-        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+        // register the service to Local Registry
+        String serviceName = UserService.class.getName();
+        LocalRegistry.register(serviceName, UserServiceImpl.class);
+
+        // register the service to Registry Center
+        RpcConfig rpcConfig = RpcApplication.getRpcConfig();
+        RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
+        Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
+        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName(serviceName);
+        serviceMetaInfo.setServiceHost(rpcConfig.getServerHost());
+        serviceMetaInfo.setServicePort(rpcConfig.getServerPort());
+
+        try{
+            registry.register(serviceMetaInfo);
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
 
         // start http server at provider side
         HttpServer httpServer = new VertxHttpServer();
