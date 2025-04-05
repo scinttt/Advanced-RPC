45dc180cba6ef767f29da793b3dddb8dbadcd240
scinttt <scinttt@gmail.com>
2025-04-05 17:21:50 -0400

    Debug TCP Client
    Implement TCP Client

---
+++ rpc-core/src/main/java/com/creaturelove/server/tcp/VertxTcpClient.java
@@ -0,0 +1,33 @@
+package com.creaturelove.server.tcp;
+
+import io.vertx.core.Vertx;
+
+public class VertxTcpClient {
+
+    public void doStart(){
+        // create Vert.x instance
+        Vertx vertx = Vertx.vertx();
+
+        vertx.createNetClient().connect(8888, "localhost", result -> {
+            if(result.succeeded()){
+                System.out.println("Connected to TCP server");
+                io.vertx.core.net.NetSocket socket = result.result();
+
+                // send data
+                socket.write("Hello, server!");
+
+                // receive response
+                socket.handler(buffer -> {
+                    System.out.println("Received response from server: " + buffer.toString());
+                });
+            }else{
+                System.err.println("Failed to connect to TCP server");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new VertxTcpClient().doStart();
+    }
+
+}
