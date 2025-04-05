a448f3709e9f712c053b0cb078ab7c0cdec56616
scinttt <scinttt@gmail.com>
2025-04-05 16:50:20 -0400

    Initialize serializer/status/type in protocol message

---
+++ rpc-core/src/main/java/com/creaturelove/protocol/ProtocolConstant.java
@@ -0,0 +1,13 @@
+package com.creaturelove.protocol;
+
+public interface ProtocolConstant
+{
+    // message header length
+    int MESSAGE_HEADER_LENGTH = 17;
+
+    // protocol magic number
+    byte PROTOCOL_MAGIC = 0x1;
+
+    // Protocol Version Number
+    byte PROTOCOL_VERSION = 0x1;
+}
---
+++ rpc-core/src/main/java/com/creaturelove/protocol/ProtocolMessageSerializerEnum.java
@@ -0,0 +1,55 @@
+package com.creaturelove.protocol;
+
+import cn.hutool.core.util.ObjectUtil;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public enum ProtocolMessageSerializerEnum {
+    JDK(0, "jdk"),
+    JSON(1, "json"),
+    KRYO(2, "kryo"),
+    HESSIAN(3, "hessian");
+
+    private final int key;
+
+    private final String value;
+
+    ProtocolMessageSerializerEnum(int key, String value){
+        this.key = key;
+        this.value = value;
+    }
+
+    // retrieve value list
+    public static List<String> getValues(){
+        return Arrays.stream(values())
+                .map(item -> item.value)
+                .collect(Collectors.toList());
+    }
+
+    // get enum based on key
+    public static ProtocolMessageSerializerEnum getEnumByKey(int key){
+        for(ProtocolMessageSerializerEnum anEnum : ProtocolMessageSerializerEnum.values()){
+            if(anEnum.key == key){
+                return anEnum;
+            }
+        }
+        return null;
+    }
+
+    // get enum based on value
+    public static ProtocolMessageSerializerEnum getEnumByValue(String value){
+        if(ObjectUtil.isEmpty(value)){
+            return null;
+        }
+        for(ProtocolMessageSerializerEnum anEnum : ProtocolMessageSerializerEnum.values()){
+            if(anEnum.value.equals(value)){
+                return anEnum;
+            }
+        }
+        return null;
+    }
+}
