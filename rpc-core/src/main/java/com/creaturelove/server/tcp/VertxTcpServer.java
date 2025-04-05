cd458562fa71fd6d2e75669850fd05722c092c3b
scinttt <scinttt@gmail.com>
2025-04-05 19:41:46 -0400

    Solve half-package & more-package through Vertx-Provided RecordParser

--- rpc-core/src/main/java/com/creaturelove/server/tcp/VertxTcpServer.java
+++ rpc-core/src/main/java/com/creaturelove/server/tcp/VertxTcpServer.java
@@ -1,43 +1,62 @@
 package com.creaturelove.server.tcp;
 
 import com.creaturelove.server.HttpServer;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetServer;
+import io.vertx.core.parsetools.RecordParser;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class VertxTcpServer implements HttpServer {
-    private byte[] handleRequest(byte[] requestData) {
-        // request logic
-        return "Hello, clinet".getBytes();
-    }
-
     @Override
     public void doStart(int port) {
-        // create vert.x instance
+        // Create Vert.x instance
         Vertx vertx = Vertx.vertx();
 
-        // create tcp server
+        // create TCP server
         NetServer server = vertx.createNetServer();
 
         // handle request
         server.connectHandler(socket -> {
-            // handle connection
-            socket.handler(buffer -> {
-                // handle byte[]
-                byte[] requestData = buffer.getBytes();
-                // customize byte[] handle logic
-                byte[] responseData = handleRequest(requestData);
-                // send response
-                socket.write(Buffer.buffer(requestData));
+            // construct parser
+            RecordParser parser = RecordParser.newFixed(8);
+            parser.setOutput(new Handler<Buffer>() {
+                // initialization
+                int size = -1;
+                // get（header + body）
+                Buffer resultBuffer = Buffer.buffer();
+
+                @Override
+                public void handle(Buffer buffer) {
+                    if (-1 == size) {
+                        // get message body length
+                        size = buffer.getInt(4);
+                        parser.fixedSizeMode(size);
+                        // write header info to result
+                        resultBuffer.appendBuffer(buffer);
+                    } else {
+                        // write body info to result
+                        resultBuffer.appendBuffer(buffer);
+                        System.out.println(resultBuffer.toString());
+                        // do it again
+                        parser.fixedSizeMode(8);
+                        size = -1;
+                        resultBuffer = Buffer.buffer();
+                    }
+                }
             });
+
+            socket.handler(parser);
         });
 
-        // start TCP server and listen the specific port
+        // start the TCP server and listen the specific port
         server.listen(port, result -> {
-            if(result.succeeded()){
-                System.out.println("TCP sever started on port: " + port);
-            }else{
-                System.out.println("Failed to start TCP server: " + result.cause());
+            if (result.succeeded()) {
+                log.info("TCP server started on port " + port);
+            } else {
+                log.info("Failed to start TCP server: " + result.cause());
             }
         });
     }
