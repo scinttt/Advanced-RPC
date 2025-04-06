b1c6a6c7cd8b06fd7a1960acbd6b402664a48cc0
scinttt <scinttt@gmail.com>
2025-04-05 20:14:56 -0400

    decouple the tcp logic from service proxy to VertxTcpClient

--- rpc-core/src/main/java/com/creaturelove/server/tcp/VertxTcpClient.java
+++ rpc-core/src/main/java/com/creaturelove/server/tcp/VertxTcpClient.java
@@ -1,34 +1,73 @@
 package com.creaturelove.server.tcp;
 
+import cn.hutool.core.util.IdUtil;
+import com.creaturelove.RpcApplication;
+import com.creaturelove.model.RpcRequest;
+import com.creaturelove.model.RpcResponse;
+import com.creaturelove.model.ServiceMetaInfo;
+import com.creaturelove.protocol.*;
 import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.net.NetClient;
+import io.vertx.core.net.NetSocket;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class VertxTcpClient {
-
-    public void doStart(){
-        // create Vert.x instance
+    public static RpcResponse doRequest(RpcRequest rpcRequest, ServiceMetaInfo serviceMetaInfo) throws InterruptedException, ExecutionException {
+        // send TCP request
         Vertx vertx = Vertx.vertx();
+        NetClient netClient = vertx.createNetClient();
+        CompletableFuture<RpcResponse> responseFuture = new CompletableFuture<>();
+        netClient.connect(serviceMetaInfo.getServicePort(), serviceMetaInfo.getServiceHost(),
+                result -> {
+                    if (!result.succeeded()) {
+                        System.err.println("Failed to connect to TCP server");
+                        return;
+                    }
+                    NetSocket socket = result.result();
+                    // send data
+                    // construct data
+                    ProtocolMessage<RpcRequest> protocolMessage = new ProtocolMessage<>();
+                    ProtocolMessage.Header header = new ProtocolMessage.Header();
+                    header.setMagic(ProtocolConstant.PROTOCOL_MAGIC);
+                    header.setVersion(ProtocolConstant.PROTOCOL_VERSION);
+                    header.setSerializer((byte) ProtocolMessageSerializerEnum.getEnumByValue(RpcApplication.getRpcConfig().getSerializer()).getKey());
+                    header.setType((byte) ProtocolMessageTypeEnum.REQUEST.getKey());
+                    // generate global request ID
+                    header.setRequestId(IdUtil.getSnowflakeNextId());
+                    protocolMessage.setHeader(header);
+                    protocolMessage.setBody(rpcRequest);
 
-        vertx.createNetClient().connect(8888, "localhost", result -> {
-            if(result.succeeded()){
-                System.out.println("Connected to TCP server");
-                io.vertx.core.net.NetSocket socket = result.result();
+                    // encode requests
+                    try {
+                        Buffer encodeBuffer = ProtocolMessageEncoder.encode(protocolMessage);
+                        socket.write(encodeBuffer);
+                    } catch (IOException e) {
+                        throw new RuntimeException("协议消息编码错误");
+                    }
 
-                // send data
-                socket.write("Hello, server!");
+                    // Receive responses
+                    TcpBufferHandlerWrapper bufferHandlerWrapper = new TcpBufferHandlerWrapper(
+                            buffer -> {
+                                try {
+                                    ProtocolMessage<RpcResponse> rpcResponseProtocolMessage =
+                                            (ProtocolMessage<RpcResponse>) ProtocolMessageDecoder.decode(buffer);
+                                    responseFuture.complete(rpcResponseProtocolMessage.getBody());
+                                } catch (IOException e) {
+                                    throw new RuntimeException("协议消息解码错误");
+                                }
+                            }
+                    );
+                    socket.handler(bufferHandlerWrapper);
 
-                // receive response
-                socket.handler(buffer -> {
-                    System.out.println("Received response from server: " + buffer.toString());
                 });
-            }else{
-                System.err.println("Failed to connect to TCP server");
-            }
-        });
-    }
 
-    public static void main(String[] args) {
-        new VertxTcpClient().doStart();
+        RpcResponse rpcResponse = responseFuture.get();
+        // close the connection
+        netClient.close();
+        return rpcResponse;
     }
-
 }
-
