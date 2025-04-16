3b47db8804a2dae0fa8ff91eaeccfd5a9f0fa970
scinttt <scinttt@gmail.com>
2025-04-16 13:21:13 -0400

    import three bootstrap class to @EnableRPC

--- rpc-spring-boot-starter/src/main/java/com/creaturelove/rpcspringbootstarter/annotation/EnableRpc.java
+++ rpc-spring-boot-starter/src/main/java/com/creaturelove/rpcspringbootstarter/annotation/EnableRpc.java
@@ -1,5 +1,10 @@
 package com.creaturelove.rpcspringbootstarter.annotation;
 
+import com.creaturelove.rpcspringbootstarter.bootstrap.RpcConsumerBootstrap;
+import com.creaturelove.rpcspringbootstarter.bootstrap.RpcInitBootstrap;
+import com.creaturelove.rpcspringbootstarter.bootstrap.RpcProviderBootstrap;
+import org.springframework.context.annotation.Import;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,6 +12,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@Import({RpcInitBootstrap.class, RpcConsumerBootstrap.class, RpcProviderBootstrap.class})
 public @interface EnableRpc {
 
     // need to start server
